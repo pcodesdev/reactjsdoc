70c3eb9da1a8aae56f5a79175d62f505bcd8bfdf
Peter Njuguna <njugunapm23@gmail.com>
2023-01-13 08:24:15 +0300

    responding to events in reactjs

--- src/practice/mybutton/MyButton.js
+++ src/practice/mybutton/MyButton.js
@@ -1,13 +1,24 @@
-import React from 'react';
-import './mybutton.css';
+import React from "react";
+import "./mybutton.css";
 
 function MyButton() {
-    return (
-        <div className='mybutton'>
-            <button className='main-btn' style={{ margin: '3rem auto', padding: '0.5rem' }}>Click Here</button> {/* inline styles */}
-
-        </div>
-    );
+  //handle click function
+  function handleClick() {
+    alert("You clicked me!");
+  }
+  return (
+    <div className="mybutton">
+      {/* Any time the button is clicked a popup window appears with the message "you clicked me" */}
+      <button
+        onClick={handleClick}
+        className="main-btn"
+        style={{ margin: "3rem auto", padding: "0.5rem" }}
+      >
+        Click Here
+      </button>{" "}
+      {/* inline styles */}
+    </div>
+  );
 }
 
 export default MyButton;
